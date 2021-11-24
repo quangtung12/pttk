c82e9fa98cd3328a3a4933ec9a54647bad60b858
unknown <phamkhacson2000@gmail.com>
2021-11-24 12:12:49 +0700

    commit Electronic

---
+++ src/main/java/com/example/sbtickets/bean/ElectronicBean.java
@@ -0,0 +1,19 @@
+package com.example.sbtickets.bean;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class ElectronicBean {
+    private String Producer;
+    private String Name;
+    private String Type;
+    private Date Producer_date;
+    private String Ram;
+    private  String Cpu;
+    private  String Screen;
+    private  String Keyboard;
+    private String image;
+}
